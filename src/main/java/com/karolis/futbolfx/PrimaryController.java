4b00550c0250fde51cbcf670801d1d1f7277205b
Karolis_portatil <karolisjakas04@gmail.com>
2024-03-03 23:04:43 +0100

    AddTeam view

--- src/main/java/com/karolis/futbolfx/PrimaryController.java
+++ src/main/java/com/karolis/futbolfx/PrimaryController.java
@@ -2,11 +2,27 @@ package com.karolis.futbolfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void addButton() throws IOException {
+        // Cargar el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("addTeam.fxml"));
+        Parent root = loader.load();
+        
+        // Crear la escena
+        Scene scene = new Scene(root);
+        
+        // Obtener el escenario principal
+        Stage stage = new Stage();
+        
+        // Establecer la escena en el escenario principal y mostrarlo
+        stage.setScene(scene);
+        stage.show();
     }
 }
