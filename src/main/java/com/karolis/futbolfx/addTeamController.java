3fb56921aef964bbd153105dd513e27a122f2697
Karolis_portatil <karolisjakas04@gmail.com>
2024-03-04 10:06:26 +0100

    Managing addTeam Exceptions

--- src/main/java/com/karolis/futbolfx/addTeamController.java
+++ src/main/java/com/karolis/futbolfx/addTeamController.java
@@ -2,33 +2,50 @@ package com.karolis.futbolfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextField;
 
 public class addTeamController {
     @FXML
     private TextField teamName, playedMatches, winnedMatches, drawMatches ;
+    Alert alert = new Alert(AlertType.INFORMATION);
+    private String name;
+    private int played, winned, draw;
 
     @FXML
     private void add() throws IOException {
         
-        String name = teamName.getText();
+        try{
+         name = teamName.getText();
+
+            if (name.isEmpty()){
+                throw new Exception(" ");
+            }
+            
+                try {
+               // Leer los valores de los TextField
+               played = Integer.parseInt(playedMatches.getText());
+               winned = Integer.parseInt(winnedMatches.getText());
+               draw = Integer.parseInt(drawMatches.getText());
+
+           } catch (NumberFormatException e) {
+               // Manejar el caso en el que uno de los valores no sea un número entero válido
+               alert.setTitle("ERROR");
+               alert.setHeaderText(null);
+               alert.setContentText("En las casillas de partidos debes introducir numeros!");
+                // Mostrar la alerta como un pop-up
+               alert.showAndWait();
+           }
+            
+        } catch (Exception e){
+        alert.setTitle("ERROR");
+        alert.setHeaderText(null);
+        alert.setContentText("Debes ponerle un nombre a este equipo!");
+        // Mostrar la alerta como un pop-up
+        alert.showAndWait();
+        }
         
-        try {
-        // Leer los valores de los TextField
-        int played = Integer.parseInt(playedMatches.getText());
-        int winned = Integer.parseInt(winnedMatches.getText());
-        int draw = Integer.parseInt(drawMatches.getText());
-
-        // Hacer algo con los valores leídos
-        System.out.println("Valor 1: " + played);
-        System.out.println("Valor 2: " + winned);
-        System.out.println("Valor 3: " + draw);
-
-    } catch (NumberFormatException e) {
-        // Manejar el caso en el que uno de los valores no sea un número entero válido
-        System.out.println("Error: uno de los valores no es un número entero válido.");
-    }
-
     }
     
 }
